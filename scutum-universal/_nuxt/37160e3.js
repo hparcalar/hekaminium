5a50a2073362b86693fe87e7a9d1181968916cbf
AhmetfKasar <ahmetfkasar@gmail.com>
2023-01-13 13:02:25 +0300

    gitIgnore modified for scutum-universal folder

--- scutum-universal/_nuxt/37160e3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[136],{1773:function(e,n,t){"use strict";t.r(n);var l={name:"PagesIssuesDetails"},c=t(3),component=Object(c.a)(l,(function(){return(0,this._self._c)("nuxt-child",{key:this.$route.params.id})}),[],!1,null,null,null);n.default=component.exports}}]);
